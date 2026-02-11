2e5fe88e9b41a353235273807c798e441d0316b5
Alumnos <Alumnos@L19-2543309>
2026-02-11 09:21:38 -0600

    Creacion del proyecto de VS

---
+++ POO/Persona.cs
@@ -0,0 +1,16 @@
+﻿public class Persona
+{
+	// Atributos
+	public string Nombre { get; set; } //Encapsulamiento
+	public int Edad { get; set; } 
+
+	//Constructor
+	public Persona(string nombre, int edad)
+	{
+		Nombre = nombre;
+		Edad = edad;
+	}
+
+	// Metodos 
+	
+}
