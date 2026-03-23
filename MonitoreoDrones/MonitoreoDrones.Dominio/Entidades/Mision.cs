cb9227239406ed645e250de339070409268fd8e4
alexislases06 <alexislases06@gmail.com>
2026-03-23 10:41:54 -0600

    DRONES

---
+++ MonitoreoDrones/MonitoreoDrones.Dominio/Entidades/Mision.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoreoDrones.Dominio.Entidades
+{
+    public class Mision
+    {
+        // Atributos
+        public int Id { get; set; }
+        public string Tipo { get; set; }
+        public int DuracionEstimada { get; set; }
+        public bool Completada { get; set; }
+
+        // Constructor
+        public Mision(int id, string tipo, int duracion)
+        {
+            Id = id;
+            Tipo = tipo;
+            DuracionEstimada = duracion;
+            Completada = false;
+        }
+
+        // Metodos
+        public void CompletarMision()
+        {
+            Completada = true;
+        }
+    }
+}
---
+++ MonitoreoDrones/MonitoreoDrones.Utilidades/UtilidadesCalculoDron.cs
@@ -0,0 +1,15 @@
+﻿namespace MonitoreoDrones.Utilidades
+{
+    public static class UtilidadesCalculoDron
+    {
+        public static double EstimarConsumoBateria(double distancia)
+        {
+            return distancia * 2.5;
+        }
+
+        public static double EstimarAutomoniaRestante(double nivelBateria)
+        {
+            return nivelBateria / 2.5;
+        }
+    }
+}
